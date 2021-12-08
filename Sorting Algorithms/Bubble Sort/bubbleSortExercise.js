052fac7047bb9147cef53c85d2b86c3b18ca8117
misha1204 <Mikheil.kvashali@gmail.com>
2021-12-08 14:45:37 +0400

    Refresher

--- Sorting Algorithms/Bubble Sort/bubbleSortExercise.js
+++ Sorting Algorithms/Bubble Sort/bubbleSortExercise.js
@@ -1,20 +1,19 @@
 const bubbleSort = (arr) => {
-  let noSwap;
+  for(let i = arr.length; i > 0; i--) {
+    let swaps = false;
 
-  for (let i = arr.length; i > 0; i--) {
-    noSwap = true;
-    for (let j = 0; j < i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        noSwap = false;
+    for(let j = 0; j < i - 1; j++) {
+      if(arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        swaps = true;
       }
     }
 
-    if (noSwap) break;
+    if(!swaps) break;
   }
 
   return arr;
-};
+}
 
 console.log(
   bubbleSort([10, 68, 5, 15, 98, 18, 315, 1100, -59, -5, 49, 57, 0, 8])
